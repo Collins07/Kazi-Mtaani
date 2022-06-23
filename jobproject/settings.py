ff504aec7a4b08ef98ad665ac69e54d58651fea5
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-24 00:17:53 +0300

    final changes

--- jobproject/settings.py
+++ jobproject/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'kazi',
-        'USER': 'acer',
-        'PASSWORD': 'password',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
         'HOST': 'localhost',
         'PORT': '',
     }
